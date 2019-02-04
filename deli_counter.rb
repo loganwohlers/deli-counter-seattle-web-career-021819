64727ea63daafc0bf631ea20d75705ca3979392c
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 02:43:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,10 @@ katz_deli=[]
 
 def line (katz_deli) 
   if(katz_deli.length==0)
-    return "The line is currently empty."
-  end
-  s="The line is currently: "
+    puts "The line is currently empty."
+
+  else
+    s="The line is currently: "
   for i in 1..(katz_deli.length)
     s+= "#{i}. katz_deli[i]"
   end
